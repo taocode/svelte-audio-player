25812ec84d1ab27e0d0296f081ac7e36ff968a68
taocode <mark@taocode.com>
2022-12-30 19:23:02 -0500

    feat(mdsvex): added mdsvex

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,22 @@
-import preprocess from "svelte-preprocess";
+import { mdsvex } from 'mdsvex';
+import mdsvexConfig from './mdsvex.config.js';
+import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    kit: {
+	extensions: ['.svelte', ...mdsvexConfig.extensions],
+
+	kit: {
 		adapter: adapter()
 	},
 
-    preprocess: [preprocess({
-        postcss: true
-    })]
+	preprocess: [
+		preprocess({
+			postcss: true
+		}),
+		mdsvex(mdsvexConfig)
+	]
 };
 
 export default config;
