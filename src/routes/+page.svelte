3737e4a343fb246de074c6a8658810ebf9627c73
taocode <mark@taocode.com>
2022-12-30 19:15:01 -0500

    feat(tailwind): added tailwindcss

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,9 @@
 <h1>Welcome to your library project</h1>
 <p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="hey">Hey</div>
+<style lang="postcss">
+.hey {
+  @apply p-12 bg-white text-black text-center;
+}
+</style>
